4bc7357684f4867f3cdc403e964edcd5eda98ed6
Code-Gale <codewhiz001@gmail.com>
2024-03-26 13:46:20 +0100

    country users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 --comments
-CREATE TABLE IF not EXISTS users(
+CREATE TABLE IF not exists users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255)
-    country ENUM('US', 'CO', 'TN')
+    country ENUM('US', 'CO', 'TN') NOT NULL
     );
