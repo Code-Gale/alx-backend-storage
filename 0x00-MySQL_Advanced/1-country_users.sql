315107d85ec7fa8d8bce6257377f9860bff1eaec
Code-Gale <codewhiz001@gmail.com>
2024-03-26 13:50:02 +0100

    country users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 --comments
-CREATE TABLE IF not exists users(
+CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
