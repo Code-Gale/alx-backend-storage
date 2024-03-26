cc207a32eff7f17e0c1bd2f6b5ed15e2950e868d
Code-Gale <codewhiz001@gmail.com>
2024-03-26 17:13:48 +0100

    other tasks

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
---comments
-CREATE TABLE IF NOT EXISTS users(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email varchar(255) NOT NULL UNIQUE,
-    name varchar(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-    );
+-- creates a table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
+);
